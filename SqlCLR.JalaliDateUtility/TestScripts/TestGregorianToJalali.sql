8cf65507447ac50972ff0430d3123619496fd094
Ehsan Mirsaeedi <mirsaeedi@outlook.com>
2016-09-22 12:26:50 +0330

    Some Tests Added

--- SqlCLR.JalaliDateUtility/TestScripts/TestGregorianToJalali.sql
+++ SqlCLR.JalaliDateUtility/TestScripts/TestGregorianToJalali.sql
@@ -1,1 +1,13 @@
-﻿select dbo.GregorianToJalali(GETDATE(),'yyyy/M/d hh:mm')
+﻿select dbo.GregorianToJalali(GETDATE(),'yy')
+
+select dbo.GregorianToJalali(GETDATE(),'yy-MM')
+
+select dbo.GregorianToJalali(GETDATE(),'yyyy/MM/dd hh:mm')
+
+select dbo.GregorianToJalali(GETDATE(),'yyyy/M/d hh:mm tt')
+
+select dbo.GregorianToJalali(GETDATE(),'yyyy-M-d HH:mm')
+
+select dbo.GregorianToJalali(GETDATE(),'MMMM dddd')
+
+select dbo.GregorianToJalali(GETDATE(),'yy MMMM dddd')
